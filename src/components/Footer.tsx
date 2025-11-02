68ea1ab40aae9e7bdf63e7ae8a472c0c3568dd82
Desethers  <raaphaelrossi@gmail.com>
2025-11-02 10:59:38 +0100

    Save current preview state

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-12">
+      <div className="footer-content max-w-[1440px] mx-auto px-[25px] py-6">
+        <div className="legal">
+          <p>© 2024 Raphaël Rossi - All Rights Reserved</p>
+        </div>
+        <div className="social-media">
+          <a href="https://www.instagram.com/raphaaelrossi/" target="_blank" rel="noreferrer">Instagram</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
